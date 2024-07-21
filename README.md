3511eecc504f45989f95b2bb5ca0d3a0344dbe9e
Teng Yun <davenkin@126.com>
2024-07-21 15:29:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+### Introduction
+- Demo project using Spring AI and Open AI.
+
+### How to run
+- `./mvnw spring-boot:run` and open `http://localhost:8080/hello-world`
+- Or run `main()` in `SpringAiOpenaiDemoApplication` in IDE directly
+
+### Tech stack
+- Spring Boot 3.3.2
+- Open AI
---
+++ src/main/java/davenkin/openai/SpringAiOpenaiDemoApplication.java
@@ -0,0 +1,13 @@
+package davenkin.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOpenaiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOpenaiDemoApplication.class, args);
+	}
+
+}
